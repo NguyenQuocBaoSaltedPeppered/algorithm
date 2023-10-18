da23d3c1d8e69dac60723ac5ea0a6c17021ae088
ADMIN-PC\Admin <nqbao2712@gmail.com>
2023-10-18 10:39:58 +0700

    update FilePath

---
+++ Models/CommonModel.Input.cs
@@ -0,0 +1,14 @@
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Algorithm.Model
+{
+    public partial class CommonModel
+    {
+        // public static double[,] LoadDataFromCSV(string csvFilePath)
+        // {
+            // Code here
+        // }
+    }
+}
